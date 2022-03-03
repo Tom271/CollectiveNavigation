46e7a8c4fb70176a928b01b63185ab7cd22a6c3f
Tom271 <trademarkhodgson@gmail.com>
2022-03-03 16:45:17 +0000

    Tidy up obsolete files

--- src/CollectiveMigration/src/plotting.jl
+++ src/CollectiveMigration/src/plotting.jl
@@ -1,7 +1,7 @@
 function plot_stopping_time_heatmap_v2(results, stat, x_param, y_param)
-    xlabels = Float64[] 
+    xlabels = Float64[]
     ylabels = Float64[]
-    stopping_times = Float64[]
+    stopping_times = Union{Float64,Missing}[]
     function stopping_time_index(row)
         idx = findfirst(x -> x <= 0.2 * row[1], row)
         return isnothing(idx) ? missing : idx
@@ -14,40 +14,41 @@ function plot_stopping_time_heatmap_v2(results, stat, x_param, y_param)
 
         y_param_value = getproperty(row.lw_config, y_param)["range"]
         push!(ylabels, y_param_value)
-        data = row.avg_df 
-        stopping_time = stopping_time_index(data[!,stat])
+        data = row.avg_df
+        stopping_time = stopping_time_index(data[!, stat])
         if ismissing(stopping_time)
             push!(stopping_times, missing)
         else
             push!(stopping_times, data.coarse_time[stopping_time])
         end
     end
+
     xlabels = unique(xlabels)
     ylabels = unique(ylabels)
-    Z = reshape(stopping_times, (length(xlabels), length(ylabels)))
+
+    Z = reshape(stopping_times, (length(ylabels), length(xlabels)))
+    @show Z
     logZ = log.(Z)
-    normalZ = Z./Z[1,1]
+    normalZ = Z ./ Z[1, 1]
     fig, ax, pltobj = GLMakie.heatmap(
         1:length(xlabels),
         1:length(ylabels),
         Z';
-        axis=(;
-            xlabel="Flow Strength",
-            ylabel="Sensing Range",
-            xticks=(1:length(xlabels),string.(xlabels)),
-            yticks=(1:length(ylabels),string.(ylabels))
-            ),
-        colormap=Reverse(:balance))
+        axis = (;
+            xlabel = "Flow Strength",
+            ylabel = "Sensing Range",
+            xticks = (1:length(xlabels), string.(xlabels)),
+            yticks = (1:length(ylabels), string.(ylabels)),
+        ),
+        colormap = Reverse(:balance),
+    )
     text!(
         string.(round.(100 .* normalZ) ./ 100)[:],
-        position = Point.(
-                (1:length(xlabels))',
-                (1:length(ylabels)),
-            )[:],
+        position = Point.((1:length(xlabels))', (1:length(ylabels)))[:],
         align = (:center, :baseline),
         color = :white,
     )
     Colorbar(fig[1, 2], pltobj, label = "Stopping Time")
     display(fig)
-    return fig,ax, pltobj
+    return fig, ax, pltobj
 end
---
+++ src/running_experiments.jl
@@ -0,0 +1,49 @@
+DrWatson.default_allowed(::SimulationConfig) = (Real, String, Symbol, Dict)
+DrWatson.allaccess(::SimulationConfig) = [
+    "sensing",
+    "flow",
+    "num_repeats",
+    "mean_run_time",
+    "num_agents",
+    "terminal_time",
+    "goal",
+    "initial_condition",
+]
+DrWatson.default_expand(::SimulationConfig) =
+    ["sensing", "flow", "goal", "initial_condition"]
+
+function run_experiment(
+    default_config::SimulationConfig,
+    flow::Symbol,
+    sensing::Symbol;
+    flow_values = 0.0:0.1:0.1,
+    sensing_values = [0.0, 500.0],
+)
+    df = DataFrame()
+    if default_config.kappa_input === nothing || default_config.kappa_CDF === nothing
+        default_config.kappa_CDF, default_config.kappa_input = load_kappa_CDF()
+    end
+    parse_config!(default_config)
+    for flow_value in flow_values
+        config = default_config
+        config.save_name = ""
+        flow_dict = getproperty(config, flow)
+        flow_dict["strength"] = flow_value
+        setproperty!(config, flow, flow_dict)
+        for sensing_value in sensing_values
+            logmessage(flow_value, sensing_value)
+            sense_dict = getproperty(config, sensing)
+            sense_dict["range"] = sensing_value
+            setproperty!(config, sensing, sense_dict)
+            # safe save not necessary as realisations are averaged over
+            file, path = produce_or_load(datadir(), config, run_many_realisations)
+            if flow_value == flow_values[1] && sensing_value == sensing_values[1]
+                df = DataFrame(file)
+            else
+                append!(df, file)
+            end
+        end
+    end
+
+    return df
+end
