27f6848677548e22951cbe4f9e0e959c6cfdf0eb
Tom271 <trademarkhodgson@gmail.com>
2022-03-03 16:45:55 +0000

    Fix dependencies and exports

--- src/CollectiveMigration/src/CollectiveMigration.jl
+++ src/CollectiveMigration/src/CollectiveMigration.jl
@@ -1,19 +1,19 @@
 module CollectiveMigration
 
-using CodecZlib, DelimitedFiles, DataFrames, Dates, Distances, Distributions
-using Interpolations, LinearAlgebra, JLD2, GLMakie, Manifolds
-using MAT, Parameters, Random
+using CodecZlib, DelimitedFiles, DataFrames, Dates, Distances, Distributions, DrWatson
+using Interpolations, LinearAlgebra, JLD2, GLMakie, Manifolds, MAT, Parameters, Random
 
-export load_kappa_CDF, get_kappa
-export check_arrivals, open_save_file
-export plot_flow_field, plot_flow_field!, get_flow_function, plot_stopping_time_heatmap
-export get_flow_function,get_sensing_kernel, get_initial_heading, get_initial_position
+export load_kappa_CDF
+export SimulationConfig, parse_config!, run_realisation, run_many_realisations
+export logmessage
+export plot_stopping_time_heatmap_v2
+
 include("circle_stats.jl")
 include("plotting.jl")
+include("dynamics.jl")
 include("flows.jl")
 include("sensing_types.jl")
 include("initial_conditions.jl")
 include("utils.jl")
-include("dynamics.jl")
 
 end # module
--- src/CollectiveNavigation.jl
+++ src/CollectiveNavigation.jl
@@ -1,16 +1,10 @@
 module CollectiveNavigation
 
 using DrWatson
-# These should be in CollectiveMigration
-using Dates, Distributions, DelimitedFiles, Distances,Manifolds, Random
 using Reexport
 @reexport using GLMakie, DataFrames
 @reexport using CollectiveMigration
-export run_realisation, run_many_realisations, run_experiment, parse_config!, open_save_file
-export SimulationConfig
-
-# const kappa_CDF, kappa_input = load_kappa_CDF();
-include("processing.jl")
-include("running_experiments_v2.jl")
+export run_experiment
+include("running_experiments.jl")
 
 end
