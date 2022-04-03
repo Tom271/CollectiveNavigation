18cf03fae63dd63adac8349f7cd07f167ff9d933
Tom271 <trademarkhodgson@gmail.com>
2022-04-03 10:39:50 +0100

    Daylight Savings throwing off my logmessage

--- src/CollectiveMigration/src/utils.jl
+++ src/CollectiveMigration/src/utils.jl
@@ -6,13 +6,13 @@ function open_save_file(save_dir, save_name)
         name = save_name
     end
     touch(save_dir * "\\$name.tsv")
-    file = open(save_dir * "\\$name.tsv", "a")
+    file = open(save_dir * "\\$name.tsv", "w")
     return file, name
 end
 
 function logmessage(flow_value, sensing_value)
     # current time
-    time = Dates.format(now(UTC), dateformat"yyyy-mm-dd HH:MM:SS")
+    time = Dates.format(now(), dateformat"yyyy-mm-dd HH:MM:SS")
 
     # memory the process is using 
     maxrss = "$(round(Sys.maxrss()/1048576, digits=2)) MiB"
@@ -40,4 +40,16 @@ function load_HYCOM_data(file_path::String)
     end
     sea = sea[data_name]
     return sea
+end
+
+function filter_trajectories(
+    lw_config::SimulationConfig;
+    sensing_range = missing,
+    flow_strength = missing,
+)::Bool
+    has_flow_strength =
+        (flow_strength === missing) ? true : (lw_config.flow["strength"] == flow_strength)
+    has_sensing_range =
+        (sensing_range === missing) ? true : (lw_config.sensing["range"] == sensing_range)
+    has_sensing_range && has_flow_strength
 end
