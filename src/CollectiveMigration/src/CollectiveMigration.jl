9490255c1161c2eae7b7114ecae9a29643d847ef
Tom271 <trademarkhodgson@gmail.com>
2022-04-03 10:41:16 +0100

    Add plot_group!, plot_individual! and others

--- src/CollectiveMigration/src/CollectiveMigration.jl
+++ src/CollectiveMigration/src/CollectiveMigration.jl
@@ -3,11 +3,15 @@ module CollectiveMigration
 using CodecZlib, DelimitedFiles, DataFrames, Dates, Distances, Distributions, DrWatson
 using Interpolations, LinearAlgebra, JLD2, GLMakie, Manifolds, MAT, Parameters, Random
 
-export load_kappa_CDF
+export load_kappa_CDF, get_kappa
 export SimulationConfig, parse_config!, run_realisation, run_many_realisations
-export logmessage
-export plot_stopping_time_heatmap_v2
-
+export logmessage, filter_trajectories
+export plot_stopping_time_heatmap, plot_animation
+export get_flow_function
+export theme!
+export plot_individual!, plot_group!, circ_resultant
+export get_sensing_kernel, find_neighbours
+export Zissou
 include("circle_stats.jl")
 include("plotting.jl")
 include("dynamics.jl")
--- src/CollectiveMigration/src/plotting.jl
+++ src/CollectiveMigration/src/plotting.jl
@@ -1,4 +1,47 @@
-function plot_stopping_time_heatmap_v2(results, stat, x_param, y_param)
+struct CyclicContainer{T} <: AbstractVector{T}
+    c::Vector{T}
+    n::Int
+end
+CyclicContainer(c) = CyclicContainer(c, 0)
+
+Base.length(c::CyclicContainer) = length(c.c)
+Base.size(c::CyclicContainer) = size(c.c)
+Base.getindex(c::CyclicContainer, i::Int) = c.c[mod1(i, length(c.c))]
+function Base.getindex(c::CyclicContainer)
+    c.n += 1
+    c[c.n]
+end
+Base.iterate(c::CyclicContainer, i = 1) = iterate(c.c, i)
+Base.getindex(c::CyclicContainer, i) = [c[j] for j in i]
+
+COLORS = [
+    "#440154"
+    "#472c7b"
+    "#3a528b"
+    "#2c728e"
+    "#20908c"
+    "#28ae7f"
+    "#5ec961"
+    "#addc30"
+    "#fde724"
+]
+Zissou = ["#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00"]
+CCOLORS = CyclicContainer(COLORS)
+LINESTYLES = CyclicContainer(["-", ":", "--", "-."])
+
+function generate_cmap(n)
+    if n > length(COLORS)
+        return :viridis
+    else
+        return cgrad(COLORS[1:n], n; categorical = true)
+    end
+end
+function theme!()
+    set_theme!(; palette = (color = COLORS,), fontsize = 26, linewidth = 5)
+end
+
+
+function plot_stopping_time_heatmap(results, stat, x_param, y_param)
     xlabels = Float64[]
     ylabels = Float64[]
     stopping_times = Union{Float64,Missing}[]
@@ -50,3 +93,93 @@ function plot_stopping_time_heatmap_v2(results, stat, x_param, y_param)
     display(fig)
     return fig, ax, pltobj
 end
+
+function plot_animation(df::DataFrame; sensing_value = missing, flow_strength = missing)
+    specific_flow_data = subset(
+        df,
+        :lw_config => ByRow(
+            x -> filter_trajectories(
+                x;
+                flow_strength = flow_strength,
+                sensing_range = sensing_value,
+            ),
+        ),
+    )
+    config = specific_flow_data.lw_config[1]
+    @show config
+    positions = readdlm(joinpath(config.save_dir, config.save_name * ".tsv"))
+    fig, ax, s = scatter(
+        convert(Vector{Float64}, positions[1, :]),
+        convert(Vector{Float64}, positions[2, :]),
+        ms = 3,
+        color = Zissou[4],
+    )
+    scatter!(
+        tuple(config.goal["location"]...);
+        markersize = 2 * config.goal["tolerance"],
+        color = Zissou[5],
+        markerspace = SceneSpace,
+    )
+    ax.autolimitaspect = 1
+
+    g = get_flow_function(config.flow)
+    f(x, y) = Point2(g(0, x, y))
+    streamplot!(f, -60..240, -50..150)
+    xlims!(-60, 240)
+    ylims!(-50, 150)
+    record(fig, plotsdir("test2.mp4"), 1:50:size(positions)[1]; framerate = 30) do i
+        delete!(ax, s)
+        trim_empty(x) = filter(i -> isa(i, Float64), x)
+        x = convert(Vector{Float64}, trim_empty(positions[i, :]))
+        y = convert(Vector{Float64}, trim_empty(positions[i+1, :]))
+        s = scatter!(x, y, markersize = 3, color = :blue)
+    end
+end
+
+# Diagram Style
+function plot_individual!(
+    ax::Any,
+    pos::Tuple{T,T} where {T},
+    heading::Real;
+    size::Real = 1,
+    colour = Zissou[1],
+    arrow_colour = Zissou[2],
+    arrow_length = 1.5,
+)
+    poly!(ax, Makie.Circle(Point2f(pos[1], pos[2]), size), color = colour)
+    arrows!(
+        ax,
+        [pos[1]],
+        [pos[2]],
+        [arrow_length * cos(heading)],
+        [arrow_length * sin(heading)],
+        color = arrow_colour,
+        arrowsize = 20,
+        linewidth = 4,
+    )
+    return nothing
+end
+
+function plot_group!(
+    ax::Any,
+    pos,
+    heading::Vector{T} where {T};
+    size::Real = 1,
+    colour = Zissou[1],
+    arrow_colour = Zissou[2],
+    arrow_length = 1.5,
+)
+    poly!(ax, Makie.Circle.(Point2f.(pos[1, :], pos[2, :]), size), color = colour)
+    arrows!(
+        ax,
+        pos[1, :],
+        pos[2, :],
+        arrow_length .* cos.(heading),
+        arrow_length .* sin.(heading),
+        color = arrow_colour,
+        arrowsize = 20,
+        linewidth = 4,
+    )
+    return nothing
+end
+
