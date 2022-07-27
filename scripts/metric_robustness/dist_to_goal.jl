a8650f37d0b5f8c14c98f9f290f053f9d7b46ff1
Tom271 <trademarkhodgson@gmail.com>
2022-07-27 09:18:49 +0100

    Reverse!
    Is this parallel computing?

--- scripts/metric_robustness/dist_to_goal.jl
+++ scripts/metric_robustness/dist_to_goal.jl
@@ -8,8 +8,8 @@ using DelimitedFiles
 @quickactivate :CollectiveNavigation
 const kappa_CDF, kappa_input = load_kappa_CDF();
 full_df = nothing
-for num_agents ∈ [30, 100, 500]
-    for tol ∈ [5.0, 10.0, 20.0]
+for num_agents ∈ reverse([30, 100, 500])
+    for tol ∈ reverse([5.0, 10.0, 20.0])
         # if num_agents > 100
         #     continue
         # end
@@ -33,7 +33,7 @@ for num_agents ∈ [30, 100, 500]
             :sensing;
             sensing_values=[0.0, 1.0, 2.0, 3.0, 5.0, 10.0, 20.0, 50.0, 500.0]
         )
-        if num_agents == 30 && tol == 5.0
+        if num_agents == 500 && tol == 20.0
             global full_df = data
         else
             append!(full_df, data)
