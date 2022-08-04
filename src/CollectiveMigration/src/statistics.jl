2fec66b82871a20ef59c952d895cf0a780104b50
Tom271 <trademarkhodgson@gmail.com>
2022-08-04 13:06:30 +0100

    Add function to calc mean individuals remaining

--- src/CollectiveMigration/src/statistics.jl
+++ src/CollectiveMigration/src/statistics.jl
@@ -1,1 +1,15 @@
+"""
+get_mean_individuals_remaining(df::DataFrame, groups::Vector{Symbol})::DataFrame
+Calculates mean individuals remaining at each time across realisations.
 
+After grouping by `groups`, calculates the mean and standard deviation
+of individuals remaining. Expected input is a `DataFrame` in the form outputted
+by `decompress_data`
+
+See also [`decompress_data`](@ref)
+"""
+function get_mean_individuals_remaining(df::DataFrame, groups::Vector{Symbol})::DataFrame
+    @pipe df |>
+          groupby(_, [:coarse_time, groups...]) |>
+          combine(_, :individuals_remaining => mean, :individuals_remaining => std)
+end
