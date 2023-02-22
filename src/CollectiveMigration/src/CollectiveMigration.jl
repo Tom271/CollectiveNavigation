7fed461d9c2cc333f16ac82a6c00f9f0e1cf9ef8
Tom271 <trademarkhodgson@gmail.com>
2023-02-22 23:41:52 +0000

    Add GeoJSON dependency

--- src/CollectiveMigration/src/CollectiveMigration.jl
+++ src/CollectiveMigration/src/CollectiveMigration.jl
@@ -1,7 +1,7 @@
 module CollectiveMigration
 
-using DelimitedFiles, DataFrames, Dates, Distances, Distributions, DrWatson
-using Interpolations, LinearAlgebra, JLD2, Makie, GLMakie, CairoMakie, Manifolds, MAT, Parameters, Random
+using DelimitedFiles, DataFrames, Dates, Distances, Distributions, Downloads, DrWatson
+using Interpolations, LinearAlgebra, JLD2, GeoJSON, JSON, Makie, GLMakie, CairoMakie, GeoMakie, Manifolds, MAT, Parameters, NetCDF, Random
 using Colors, PerceptualColourMaps
 using Pipe: @pipe
 using ProgressBars
@@ -9,6 +9,7 @@ using ProgressBars
 export load_kappa_CDF, get_kappa
 export SimulationConfig, parse_config!, run_realisation, run_many_realisations
 export logmessage, filter_trajectories, decompress_data
+export HYCOM_Parameters, get_flow_data, HYCOM_Flow_Data, sanitise_flow_data!, build_interpolants!
 export plot_stopping_time_heatmap, plot_animation_v2
 export get_flow_function
 export theme!
@@ -18,6 +19,7 @@ export Zissou
 export get_stopping_times
 export get_mean_individuals_remaining, get_arrival_times, get_centile_arrival
 export make_failures_explicit, plot_arrival_heatmap, plot_one_density, plot_averages
+
 include("circle_stats.jl")
 include("dynamics.jl")
 include("flows.jl")
@@ -25,6 +27,7 @@ include("sensing_types.jl")
 include("initial_conditions.jl")
 include("statistics.jl")
 include("utils.jl")
+include("hycom.jl")
 include("plotting.jl")
 
 end # module
